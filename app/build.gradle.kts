22c13fc3e07c692a122ef9f13b3c5c518545bb08
Marko Mihajlović <markomihajlovicmare01@gmail.com>
2025-11-01 02:41:42 +0100

    - Refactoring app package name and package references.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.shadowlinkvpn"
+    namespace = "net.libreguard.vpn"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.shadowlinkvpn"
+        applicationId = "net.libreguard.vpn"
         minSdk = 29
         targetSdk = 35
         versionCode = 1
--- app/src/main/java/com/example/shadowlinkvpn/ShadowLinkVpnApp.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shadowlinkvpn
-import org.strongswan.android.logic.StrongSwanApplication
-
-class ShadowLinkVpnApp : StrongSwanApplication() {
-    override fun onCreate() {
-        super.onCreate()
-        // Application-specific initialization can go here
-    }
-}
