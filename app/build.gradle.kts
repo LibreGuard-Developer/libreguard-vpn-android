75999ffb12401ca804cf1027c50f9c98a3061ea0
Marko Mihajlović <markomihajlovicmare01@gmail.com>
2026-05-29 11:05:09 +0200

    Bump version to 1.9.0 and update version code to 10900

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -160,8 +160,8 @@ android {
         applicationId = "net.libreguard.vpn"
         minSdk = 29
         targetSdk = 35
-        versionCode = 10880
-        versionName = "1.8.8"
+        versionCode = 10900
+        versionName = "1.9.0"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
